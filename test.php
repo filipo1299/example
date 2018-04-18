775cfd7097dffb5372452bd2694dd157b15324b1
Filip <filip@filip>
2018-04-18 10:35:17 +0200

    test.php added

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 <?php
-$table=array();
+function generuj_zbior($ilosc){
 
+    $ile=0;
+    $zbior_liczb=array();
+    while($ile<$ilosc){
+        $ile++;
+        $zbior_liczb[]=$ile;
+    }
+    return $zbior_liczb;
+    $zbior = generuj_zbior(50);
+    echo '<pre>';
+    print_r($zbior);
+    echo '</pre>';
 ?>
