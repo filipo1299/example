e939cddacf0026e8476788393c55eaa0d725125b
Filip <filip@filip>
2018-04-18 12:00:24 +0200

    test.php added

--- test.php
+++ test.php
@@ -1,11 +1,20 @@
 <?php
-$liczby = array();
+//zad1
+$numbers = array();
 
-for ($i=0 ; $i<=100 ;$i++ )
-{
-    $liczby[$i] = rand(0,100);
-    for ($j=0 ; $j<$i ; ++$j)
-        if ( $liczby[$j] == $liczby[$i] ) --$i;
+for ($i = 0; $i <= 100; $i++) {
+    $numbers[$i] = rand(0, 100);
+    for ($j = 0; $j < $i; ++$j)
+        if ($numbers[$j] == $numbers[$i]) --$i;
 
 }
-print_r($liczby);
+print_r($numbers);
+////zad2.
+$tab[][] = "";
+
+for ($a = 0; $a <= 100; $a++) {
+    for ($b = 0; $b <= 100; $b++) {
+        $tab[$a][$b] = rand(100, 1000);
+    }
+}
+print_r($tab);
